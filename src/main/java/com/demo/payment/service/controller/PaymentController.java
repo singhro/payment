1bdf855ea9653f036579482b521657457d1cb7ff
Rohit Singh <singh.ro@HCL.COM>
2020-05-05 16:50:00 +0530

    code

--- src/main/java/com/demo/payment/service/controller/PaymentController.java
+++ src/main/java/com/demo/payment/service/controller/PaymentController.java
@@ -1,7 +1,5 @@
 package com.demo.payment.service.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,20 +11,17 @@ import java.util.Set;
 @RestController
 public class PaymentController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(PaymentController.class);
-
-
     @GetMapping("/test/payments")
     public String test(){
-        LOG.info("Test Method");
-        LOG.debug("Test Method");
+        System.out.println("Test Method");
+        System.err.println("Test Method");
         return "Get Payment";
     }
 
     @PostMapping("/test/payments")
     public String createPayment(@RequestBody Map<Object,Object> attributes){
-        LOG.info("Create Payment Method "+attributes);
-        LOG.debug("Create Payment Method " + attributes);
+        System.out.println("Create Payment Method "+attributes);
+        System.err.println("Create Payment Method " + attributes);
         String response = "<html><body><h1>Payment Success</h1></body</html>";
         return response;
     }
